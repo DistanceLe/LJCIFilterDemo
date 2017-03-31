58f81ac2955f2f6234e4eb48f09888105971acf6
lijie <rd32@ce-link.com>
2017-03-31 14:34:25 +0800

    继续添加 滤镜类别

--- CIImageDemo/LJFilterConst.h
+++ CIImageDemo/LJFilterConst.h
@@ -143,5 +143,213 @@ UIKIT_EXTERN NSString *const LJCILineScreen;
 
 
 #pragma mark - ================ CICategoryColorAdjustment(颜色调节) ==================
+/**  让颜色保持在一个指定范围 inputImage  inputMinComponents [0 0 0 0]  inputMaxComponents[1 1 1 1]*/
+UIKIT_EXTERN NSString *const LJCIColorClamp;
+/**  调节颜色 inputImage  inputSaturation(饱和度)  inputBrightness  inputContrast(对比度) */
+UIKIT_EXTERN NSString *const LJCIColorControls;
+/**  matrix(矩阵 RGBA)  inputImage  inputRVector[1 0 0 0]  inputGVector[0 1 0 0]  inputBVector[0 0 1 0]  inputAVector[0 0 0 1]  inputBiasVector[0 0 0 0](偏斜率)
+     s.r = dot(s, redVector)  s = s + bias*/
+UIKIT_EXTERN NSString *const LJCIColorMatrix;
+/**  (polynomial(多项)coefficient(系数)） inputImage  inputRedCoefficients[0 1 0 0]  inputGreenCoefficients[0 1 0 0]  inputBlueCoefficients[0 1 0 0]    inputAlphaCoefficients[0 1 0 0]
+     r = rCoeff[0] + rCoeff[1] * r + rCoeff[2] * r*r + rCoeff[3] * r*r*r */
+UIKIT_EXTERN NSString *const LJCIColorPolynomial;
+/**  (exposure 曝光)  inputImage  inputEV   s.rgb * pow(2.0, ev)*/
+UIKIT_EXTERN NSString *const LJCIExposureAdjust;
+/**  (gamma 伽玛) inputImage  inputPower    pow(s.rgb, vec3(power)) */
+UIKIT_EXTERN NSString *const LJCIGammaAdjust;
+/**  (hue 色调 色彩)  inputImage  inputAngle */
+UIKIT_EXTERN NSString *const LJCIHueAdjust;
+/**  (toneCurve 色度曲线)  inputImage （Maps color intensity from a linear gamma curve to the sRGB color space.）*/
+UIKIT_EXTERN NSString *const LJCILinearToSRGBToneCurve;
+/**  inputImage  (Maps color intensity from the sRGB color space to a linear gamma curve) */
+UIKIT_EXTERN NSString *const LJCISRGBToneCurveToLinear;
+/**  色温 色彩(neutral 中性)， inputImage  inputNeutral[6500, 0]  inputTargetNeutral [6500, 0]  */
+UIKIT_EXTERN NSString *const LJCITemperatureAndTint;
+/**  色度曲线  inputImage (CIAttributeTypeOffset) inputPoint0[0, 0]  inputPoint1[0.25, 0.25]  inputPoint2[0.5, 0.5]  inputPoint3[0.75, 0.75]  inputPoint4[1, 1] */
+UIKIT_EXTERN NSString *const LJCIToneCurve;
+/**  (vibrance 震动)调整饱和度  inputImage  inputAmount */
+UIKIT_EXTERN NSString *const LJCIVibrance;
+/**  inputImage  inputColor(CIColor) */
+UIKIT_EXTERN NSString *const LJCIWhitePointAdjust;
+
+
+#pragma mark - ================ CICategoryColorEffect ==================
+/**  inputImage  inputRedCoefficients  inputGreenCoefficients  inputBlueCoefficients
+ out.r =   in.r * rC[0] +        in.g * rC[1] +        in.b * rC[2]
+         + in.r * in.r * rC[3] + in.g * in.g * rC[4] + in.b * in.b * rC[5]
+         + in.r * in.g * rC[6] + in.g * in.b * rC[7] + in.b * in.r * rC[8]
+         + rC[9]*/
+UIKIT_EXTERN NSString *const LJCIColorCrossPolynomial;
+/**  立方体  inputImage  inputCubeDimension  inputCubeData(NSData) */
+UIKIT_EXTERN NSString *const LJCIColorCube;
+/**  inputImage  inputCubeDimension  inputCubeData  inputColorSpace(CGColorSpaceRef)  */
+UIKIT_EXTERN NSString *const LJCIColorCubeWithColorSpace;
+/**  反转 inputImage */
+UIKIT_EXTERN NSString *const LJCIColorInvert;
+/**  inputImage  inputGradientImage(梯度图片) */
+UIKIT_EXTERN NSString *const LJCIColorMap;
+/**  monochrome黑白照片  inputImage  inputColor  inputIntensity（程度） */
+UIKIT_EXTERN NSString *const LJCIColorMonochrome;
+/**  (posterize 色调分离) inputImage  inputLevels*/
+UIKIT_EXTERN NSString *const LJCIColorPosterize;
+/**  inputImage  inputColor0  inputColor1 */
+UIKIT_EXTERN NSString *const LJCIFalseColor;
+/**  inputImage   */
+UIKIT_EXTERN NSString *const LJCIMaskToAlpha;
+/**  inputImage   Returns a grayscale image from max(r,g,b).*/
+UIKIT_EXTERN NSString *const LJCIMaximumComponent;
+/**  inputImage   Returns a grayscale image from min(r,g,b).*/
+UIKIT_EXTERN NSString *const LJCIMinimumComponent;
+/**  (chrome 铬)inputImage    */
+UIKIT_EXTERN NSString *const LJCIPhotoEffectChrome;
+/**  (fade 褪色) inputImage */
+UIKIT_EXTERN NSString *const LJCIPhotoEffectFade;
+/**  (instant 即刻)有点复古的样子 inputImage */
+UIKIT_EXTERN NSString *const LJCIPhotoEffectInstant;
+/**  (mono 单通道)黑白照片 inputImage*/
+UIKIT_EXTERN NSString *const LJCIPhotoEffectMono;
+/**  (noir 悲剧色彩) inputImage */
+UIKIT_EXTERN NSString *const LJCIPhotoEffectNoir;
+/**  (强调冷色) inputImage */
+UIKIT_EXTERN NSString *const LJCIPhotoEffectProcess;
+/**  (tonal 色调)也是黑白的样子 inputImage */
+UIKIT_EXTERN NSString *const LJCIPhotoEffectTonal;
+/**  (强调暖色调)inputImage */
+UIKIT_EXTERN NSString *const LJCIPhotoEffectTransfer;
+/**  sepia 深褐色  inputImage  inputIntensity */
+UIKIT_EXTERN NSString *const LJCISepiaTone;
+/**  (thermal 热的)inputImage */
+UIKIT_EXTERN NSString *const LJCIThermal;//iOS 10
+/**  vignette晕映  inputImage  inputRadius  inputIntensity */
+UIKIT_EXTERN NSString *const LJCIVignette;
+/**  inputImage  inputCenter  inputIntensity  inputRadius */
+UIKIT_EXTERN NSString *const LJCIVignetteEffect;
+/**  inputImage X射线 */
+UIKIT_EXTERN NSString *const LJCIXRay;//iOS 10
+
+
+#pragma mark - ================ CICategoryTransition(转变) ==================
+/**  (accordion手风琴 fold折叠）  inputImage  inputTargetImage  inputBottomHeight  inputNumberOfFolds  inputFoldShadowAmount   inputTime*/
+UIKIT_EXTERN NSString *const LJCIAccordionFoldTransition;//iOS 8
+/**  inputImage  inputTargetImage  inputAngle  inputWidth  inputBarOffset(NSNumber)  inputTime */
+UIKIT_EXTERN NSString *const LJCIBarsSwipeTransition;
+/**  inputImage  inputTargetImage  inputExtent  inputColor  inputTime  inputAngle   inputWidth  inputOpacity */
+UIKIT_EXTERN NSString *const LJCICopyMachineTransition;
+/** (disintegrate 崩溃) inputImage  inputTargetImage  inputMaskImage  inputTime  inputShadowRadius  inputShadowDensity  inputShadowOffset[0 -10] */
+UIKIT_EXTERN NSString *const LJCIDisintegrateWithMaskTransition;
+/**  (dissolve 溶解)  inputImage  inputTargetImage  inputTime*/
+UIKIT_EXTERN NSString *const LJCIDissolveTransition;
+/**  inputImage  inputTargetImage  inputCenter  inputExtent  inputColor  inputTime  inputMaxStriationRadius  inputStriationStrength  inputStriationContrast  inputFadeThreshold */
+UIKIT_EXTERN NSString *const LJCIFlashTransition;
+/**  inputImage  inputTargetImage  inputCenter  inputTime  inputAngle  inputRadius  inputCompression */
+UIKIT_EXTERN NSString *const LJCIModTransition;
+/**  inputImage  inputTargetImage  inputBacksideImage  inputShadingImage  inputExtent  inputTime  inputAngle  inputRadius */
+UIKIT_EXTERN NSString *const LJCIPageCurlTransition;//iOS 9
+/**  inputImage  inputTargetImage  inputBacksideImage  inputExtent  inputTime  inputAngle  inputRadius  inputShadowSize  inputShadowAmount  inputShadowExtent */
+UIKIT_EXTERN NSString *const LJCIPageCurlWithShadowTransition;//iOS 9
+/**  (ripple 波纹)inputImage  inputTargetImage   inputShadingImage  inputCenter  inputExtent  inputTime  inputWidth  inputScale */
+UIKIT_EXTERN NSString *const LJCIRippleTransition;//iOS 9
+/**  inputImage  inputTargetImage  inputExtent  inputColor  inputTime   inputAngle  inputWidth  inputOpacity*/
+UIKIT_EXTERN NSString *const LJCISwipeTransition;
+
+
+#pragma mark - ================ CICategoryTileEffect( 瓦片 ) ==================
+/**  inputImage  inputTransform */
+UIKIT_EXTERN NSString *const LJCIAffineClamp;
+/**  inputImage  inputTransform  */
+UIKIT_EXTERN NSString *const LJCIAffineTile;
+/**  inputImage  inputExtent */
+UIKIT_EXTERN NSString *const LJCIClamp;//iOS 10;
+/**  inputImage  inputCenter  inputAngle  inputWidth */
+UIKIT_EXTERN NSString *const LJCIEightfoldReflectedTile;
+/**  inputImage  inputCenter  inputAngle  inputAcuteAngle(acute 急性的)  inputWidth */
+UIKIT_EXTERN NSString *const LJCIFourfoldReflectedTile;
+/**  inputImage  inputCenter  inputAngle  inputWidth */
+UIKIT_EXTERN NSString *const LJCIFourfoldRotatedTile;
+/**  inputImage  inputCenter  inputAngle  inputAcuteAngle(acute 急性的)  inputWidth */
+UIKIT_EXTERN NSString *const LJCIFourfoldTranslatedTile;
+/**  (glide 滑翔)inputImage  inputCenter  inputAngle  inputWidth  */
+UIKIT_EXTERN NSString *const LJCIGlideReflectedTile;
+/**  (Kaleidoscope 万花筒)  inputImage  inputCount  inputCenter  inputAngle*/
+UIKIT_EXTERN NSString *const LJCIKaleidoscope;//iOS 9
+/**  (欧普艺术（OP Art）又被称为视幻艺术或光效应艺术，是使用光学的技术营造出奇异的艺术效果)inputImage  inputCenter  inputScale  inputAngle  inputWidth */
+UIKIT_EXTERN NSString *const LJCIOpTile;//iOS 9
+/**  (parallelogram 平行四边形) inputImage  inputCenter  inputAngle  inputAcuteAngle  inputWidth */
+UIKIT_EXTERN NSString *const LJCIParallelogramTile;//iOS 9
+/**  (perspective 透视)inputImage  (CIAttributeTypePosition)inputTopLeft  inputTopRight  inputBottomRight  inputBottomLeft */
+UIKIT_EXTERN NSString *const LJCIPerspectiveTile;
+/**  (Sixfold 六重) inputImage  inputCenter  inputAngle  inputWidth */
+UIKIT_EXTERN NSString *const LJCISixfoldReflectedTile;
+/**  inputImage  inputCenter  inputAngle  inputWidth */
+UIKIT_EXTERN NSString *const LJCISixfoldRotatedTile;
+/**  (三角形万花筒)inputImage  inputPoint  inputSize  inputRotation  inputDecay(衰退 腐烂) */
+UIKIT_EXTERN NSString *const LJCITriangleKaleidoscope;
+/**  inputImage  inputCenter  inputAngle  inputWidth */
+UIKIT_EXTERN NSString *const LJCITriangleTile;//iOS 9
+/**  inputImage  inputCenter  inputAngle  inputWidth */
+UIKIT_EXTERN NSString *const LJCITwelvefoldReflectedTile;
+
+
+#pragma mark - ================ CICategoryGenerator(发生器 无inputImage) ==================
+/**  (aztec code 一种二维码) inputMessage(NSData)  inputCorrectionLevel(5~95)  inputLayers(32~1)  inputCompactStyle(0~1) */
+UIKIT_EXTERN NSString *const LJCIAztecCodeGenerator;//iOS 8
+/**  (棋盘) inputCenter  inputColor0  inputColor1  inputWidth  inputSharpness（锐度） */
+UIKIT_EXTERN NSString *const LJCICheckerboardGenerator;
+/**  (条形码) inputMessage  inputQuietSpace */
+UIKIT_EXTERN NSString *const LJCICode128BarcodeGenerator;//iOS 8
+/**  (纯色图片) inputColor  */
+UIKIT_EXTERN NSString *const LJCIConstantColorGenerator;
+/**  (lenticular透镜状 halo光环)inputCenter  inputColor  inputHaloOverlap  inputHaloRadius  inputHaloWidth  inputStriationContrast  inputStriationStrength(条纹强度)  inputTime*/
+UIKIT_EXTERN NSString *const LJCILenticularHaloGenerator;//iOS 9
+/** (PDF417条码 高密度、高信息含量的) inputMessage  inputMinWidth  inputMaxWidth  inputMinHeight  inputMaxHeight  inputDataColumns  inputRows   inputPreferredAspectRatio  inputCompactionMode  inputCompactStyle  inputCorrectionLevel(0~8)  inputAlwaysSpecifyCompaction*/
+UIKIT_EXTERN NSString *const LJCIPDF417BarcodeGenerator;//iOS 9
+/**  inputMessage  inputCorrectionLevel(NSString level L, M, Q, or H) */
+UIKIT_EXTERN NSString *const LJCIQRCodeGenerator;//iOS 7
+/**  无属性 生成随机图 */
+UIKIT_EXTERN NSString *const LJCIRandomGenerator;
+/**  inputCenter  inputColor  inputRadius  inputCrossScale(0~100)  inputCrossAngle  inputCrossOpacity(0~-8)  inputCrossWidth(0.5~10)  inputEpsilon(0~-8) */
+UIKIT_EXTERN NSString *const LJCIStarShineGenerator;
+/**  (stripes 条纹布) inputCenter  inputColor0  inputColor1  inputWidth  inputSharpness */
+UIKIT_EXTERN NSString *const LJCIStripesGenerator;
+/** (阳光)(striation 条纹 contrast 对比) inputCenter  inputColor  inputSunRadius  inputMaxStriationRadius(0~10)  inputStriationStrength(0~3)  inputStriationContrast(0~5)  inputTime */
+UIKIT_EXTERN NSString *const LJCISunbeamsGenerator;//iOS 9
+
+
+#pragma mark - ================ CICategoryReduction(减少) ==================
 ///**  <#Description#> */
 //UIKIT_EXTERN NSString *const LJ
+///**  <#Description#> */
+//UIKIT_EXTERN NSString *const LJ
+///**  <#Description#> */
+//UIKIT_EXTERN NSString *const LJ
+///**  <#Description#> */
+//UIKIT_EXTERN NSString *const LJ
+///**  <#Description#> */
+//UIKIT_EXTERN NSString *const LJ
+///**  <#Description#> */
+//UIKIT_EXTERN NSString *const LJ
+///**  <#Description#> */
+//UIKIT_EXTERN NSString *const LJ
+///**  <#Description#> */
+//UIKIT_EXTERN NSString *const LJ
+///**  <#Description#> */
+//UIKIT_EXTERN NSString *const LJ
+///**  <#Description#> */
+//UIKIT_EXTERN NSString *const LJ
+///**  <#Description#> */
+//UIKIT_EXTERN NSString *const LJ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CIImageDemo/LJFilterConst.m
+++ CIImageDemo/LJFilterConst.m
@@ -132,3 +132,182 @@ NSString *const LJCIDotScreen = @"CIDotScreen";
 NSString *const LJCIHatchedScreen = @"CIHatchedScreen";
 /**  inputImage  inputCenter  inputAngle  inputWidth  inputSharpness */
 NSString *const LJCILineScreen = @"CILineScreen";
+
+#pragma mark - ================ CICategoryColorAdjustment(颜色调节) ==================
+/**  让颜色保持在一个指定范围 inputImage  inputMinComponents [0 0 0 0]  inputMaxComponents[1 1 1 1]*/
+NSString *const LJCIColorClamp = @"CIColorClamp";
+/**  调节颜色 inputImage  inputSaturation(饱和度)  inputBrightness  inputContrast(对比度) */
+NSString *const LJCIColorControls = @"CIColorControls";
+/**  matrix(矩阵 RGBA)  inputImage  inputRVector[1 0 0 0]  inputGVector[0 1 0 0]  inputBVector[0 0 1 0]  inputAVector[0 0 0 1]  inputBiasVector[0 0 0 0](偏斜率)
+ s.r = dot(s, redVector)  s = s + bias*/
+NSString *const LJCIColorMatrix = @"CIColorMatrix";
+/**  (polynomial(多项)coefficient(系数)） inputImage  inputRedCoefficients[0 1 0 0]  inputGreenCoefficients[0 1 0 0]  inputBlueCoefficients[0 1 0 0]    inputAlphaCoefficients[0 1 0 0]
+ r = rCoeff[0] + rCoeff[1] * r + rCoeff[2] * r*r + rCoeff[3] * r*r*r */
+NSString *const LJCIColorPolynomial = @"CIColorPolynomial";
+/**  (exposure 曝光)  inputImage  inputEV   s.rgb * pow(2.0, ev)*/
+NSString *const LJCIExposureAdjust = @"CIExposureAdjust";
+/**  (gamma 伽玛) inputImage  inputPower    pow(s.rgb, vec3(power)) */
+NSString *const LJCIGammaAdjust = @"CIGammaAdjust";
+/**  (hue 色调 色彩)  inputImage  inputAngle */
+NSString *const LJCIHueAdjust = @"CIHueAdjust";
+/**  (toneCurve 色度曲线)  inputImage （Maps color intensity from a linear gamma curve to the sRGB color space.）*/
+NSString *const LJCILinearToSRGBToneCurve = @"CILinearToSRGBToneCurve";
+/**  inputImage  (Maps color intensity from the sRGB color space to a linear gamma curve) */
+NSString *const LJCISRGBToneCurveToLinear = @"CISRGBToneCurveToLinear";
+/**  色温 色彩(neutral 中性)， inputImage  inputNeutral[6500, 0]  inputTargetNeutral [6500, 0]  */
+NSString *const LJCITemperatureAndTint = @"CITemperatureAndTint";
+/**  色度曲线  inputImage (CIAttributeTypeOffset) inputPoint0[0, 0]  inputPoint1[0.25, 0.25]  inputPoint2[0.5, 0.5]  inputPoint3[0.75, 0.75]  inputPoint4[1, 1] */
+NSString *const LJCIToneCurve = @"CIToneCurve";
+/**  (vibrance 震动)调整饱和度  inputImage  inputAmount */
+NSString *const LJCIVibrance = @"CIVibrance";
+/**  inputImage  inputColor(CIColor) */
+NSString *const LJCIWhitePointAdjust = @"CIWhitePointAdjust";
+
+
+#pragma mark - ================ CICategoryColorEffect ==================
+/**  inputImage  inputRedCoefficients  inputGreenCoefficients  inputBlueCoefficients
+ out.r =   in.r * rC[0] +        in.g * rC[1] +        in.b * rC[2]
+ + in.r * in.r * rC[3] + in.g * in.g * rC[4] + in.b * in.b * rC[5]
+ + in.r * in.g * rC[6] + in.g * in.b * rC[7] + in.b * in.r * rC[8]
+ + rC[9]*/
+NSString *const LJCIColorCrossPolynomial = @"CIColorCrossPolynomial";
+/**  立方体  inputImage  inputCubeDimension  inputCubeData(NSData) */
+NSString *const LJCIColorCube = @"CIColorCube";
+/**  inputImage  inputCubeDimension  inputCubeData  inputColorSpace(CGColorSpaceRef)  */
+NSString *const LJCIColorCubeWithColorSpace = @"CIColorCubeWithColorSpace";
+/**  反转 inputImage */
+NSString *const LJCIColorInvert = @"CIColorInvert";
+/**  inputImage  inputGradientImage(梯度图片) */
+NSString *const LJCIColorMap = @"CIColorMap";
+/**  monochrome黑白照片  inputImage  inputColor  inputIntensity（程度） */
+NSString *const LJCIColorMonochrome = @"CIColorMonochrome";
+/**  (posterize 色调分离) inputImage  inputLevels*/
+NSString *const LJCIColorPosterize = @"CIColorPosterize";
+/**  inputImage  inputColor0  inputColor1 */
+NSString *const LJCIFalseColor = @"CIFalseColor";
+/**  inputImage   */
+NSString *const LJCIMaskToAlpha = @"CIMaskToAlpha";
+/**  inputImage   Returns a grayscale image from max(r,g,b).*/
+NSString *const LJCIMaximumComponent = @"CIMaximumComponent";
+/**  inputImage   Returns a grayscale image from min(r,g,b).*/
+NSString *const LJCIMinimumComponent = @"CIMinimumComponent";
+/**  (chrome 铬)inputImage    */
+NSString *const LJCIPhotoEffectChrome = @"CIPhotoEffectChrome";
+/**  (fade 褪色) inputImage */
+NSString *const LJCIPhotoEffectFade = @"CIPhotoEffectFade";
+/**  (instant 即刻)有点复古的样子 inputImage */
+NSString *const LJCIPhotoEffectInstant = @"CIPhotoEffectInstant";
+/**  (mono 单通道)黑白照片 inputImage*/
+NSString *const LJCIPhotoEffectMono = @"CIPhotoEffectMono";
+/**  (noir 悲剧色彩) inputImage */
+NSString *const LJCIPhotoEffectNoir = @"CIPhotoEffectNoir";
+/**  (强调冷色) inputImage */
+NSString *const LJCIPhotoEffectProcess = @"CIPhotoEffectProcess";
+/**  (tonal 色调)也是黑白的样子 inputImage */
+NSString *const LJCIPhotoEffectTonal = @"CIPhotoEffectTonal";
+/**  (强调暖色调)inputImage */
+NSString *const LJCIPhotoEffectTransfer = @"CIPhotoEffectTransfer";
+/**  sepia 深褐色  inputImage  inputIntensity */
+NSString *const LJCISepiaTone = @"CISepiaTone";
+/**  (thermal 热的)inputImage */
+NSString *const LJCIThermal = @"CIThermal";//iOS 10
+/**  vignette晕映  inputImage  inputRadius  inputIntensity */
+NSString *const LJCIVignette = @"CIVignette";
+/**  inputImage  inputCenter  inputIntensity  inputRadius */
+NSString *const LJCIVignetteEffect = @"CIVignetteEffect";
+/**  inputImage X射线 */
+NSString *const LJCIXRay = @"CIXRay";//iOS 10
+
+
+#pragma mark - ================ CICategoryTransition(转变) ==================
+/**  (accordion手风琴 fold折叠）  inputImage  inputTargetImage  inputBottomHeight  inputNumberOfFolds  inputFoldShadowAmount   inputTime*/
+NSString *const LJCIAccordionFoldTransition = @"CIAccordionFoldTransition";//iOS 8
+/**  inputImage  inputTargetImage  inputAngle  inputWidth  inputBarOffset(NSNumber)  inputTime */
+NSString *const LJCIBarsSwipeTransition = @"CIBarsSwipeTransition";
+/**  inputImage  inputTargetImage  inputExtent  inputColor  inputTime  inputAngle   inputWidth  inputOpacity */
+NSString *const LJCICopyMachineTransition = @"CICopyMachineTransition";
+/** (disintegrate 崩溃) inputImage  inputTargetImage  inputMaskImage  inputTime  inputShadowRadius  inputShadowDensity  inputShadowOffset[0 -10] */
+NSString *const LJCIDisintegrateWithMaskTransition = @"CIDisintegrateWithMaskTransition";
+/**  (dissolve 溶解)  inputImage  inputTargetImage  inputTime*/
+NSString *const LJCIDissolveTransition = @"CIDissolveTransition";
+/**  inputImage  inputTargetImage  inputCenter  inputExtent  inputColor  inputTime  inputMaxStriationRadius  inputStriationStrength  inputStriationContrast  inputFadeThreshold */
+NSString *const LJCIFlashTransition = @"CIFlashTransition";
+/**  inputImage  inputTargetImage  inputCenter  inputTime  inputAngle  inputRadius  inputCompression */
+NSString *const LJCIModTransition = @"CIModTransition";
+/**  inputImage  inputTargetImage  inputBacksideImage  inputShadingImage  inputExtent  inputTime  inputAngle  inputRadius */
+NSString *const LJCIPageCurlTransition = @"CIPageCurlTransition";//iOS 9
+/**  inputImage  inputTargetImage  inputBacksideImage  inputExtent  inputTime  inputAngle  inputRadius  inputShadowSize  inputShadowAmount  inputShadowExtent */
+NSString *const LJCIPageCurlWithShadowTransition = @"CIPageCurlWithShadowTransition";//iOS 9
+/**  (ripple 波纹)inputImage  inputTargetImage   inputShadingImage  inputCenter  inputExtent  inputTime  inputWidth  inputScale */
+NSString *const LJCIRippleTransition = @"CIRippleTransition";//iOS 9
+/**  inputImage  inputTargetImage  inputExtent  inputColor  inputTime   inputAngle  inputWidth  inputOpacity*/
+NSString *const LJCISwipeTransition = @"CISwipeTransition";
+
+
+#pragma mark - ================ CICategoryTileEffect( 瓦片 ) ==================
+/**  inputImage  inputTransform */
+NSString *const LJCIAffineClamp = @"CIAffineClamp";
+/**  inputImage  inputTransform  */
+NSString *const LJCIAffineTile = @"CIAffineTile";
+/**  inputImage  inputExtent */
+NSString *const LJCIClamp = @"CIClamp";//iOS 10;
+/**  inputImage  inputCenter  inputAngle  inputWidth */
+NSString *const LJCIEightfoldReflectedTile = @"CIEightfoldReflectedTile";
+/**  inputImage  inputCenter  inputAngle  inputAcuteAngle(acute 急性的)  inputWidth */
+NSString *const LJCIFourfoldReflectedTile = @"CIFourfoldReflectedTile";
+/**  inputImage  inputCenter  inputAngle  inputWidth */
+NSString *const LJCIFourfoldRotatedTile = @"CIFourfoldRotatedTile";
+/**  inputImage  inputCenter  inputAngle  inputAcuteAngle(acute 急性的)  inputWidth */
+NSString *const LJCIFourfoldTranslatedTile = @"CIFourfoldTranslatedTile";
+/**  (glide 滑翔)inputImage  inputCenter  inputAngle  inputWidth  */
+NSString *const LJCIGlideReflectedTile = @"CIGlideReflectedTile";
+/**  (Kaleidoscope 万花筒)  inputImage  inputCount  inputCenter  inputAngle*/
+NSString *const LJCIKaleidoscope = @"CIKaleidoscope";//iOS 9
+/**  (欧普艺术（OP Art）又被称为视幻艺术或光效应艺术，是使用光学的技术营造出奇异的艺术效果)inputImage  inputCenter  inputScale  inputAngle  inputWidth */
+NSString *const LJCIOpTile = @"CIOpTile";//iOS 9
+/**  (parallelogram 平行四边形) inputImage  inputCenter  inputAngle  inputAcuteAngle  inputWidth */
+NSString *const LJCIParallelogramTile = @"CIParallelogramTile";//iOS 9
+/**  (perspective 透视)inputImage  (CIAttributeTypePosition)inputTopLeft  inputTopRight  inputBottomRight  inputBottomLeft */
+NSString *const LJCIPerspectiveTile = @"CIPerspectiveTile";
+/**  (Sixfold 六重) inputImage  inputCenter  inputAngle  inputWidth */
+NSString *const LJCISixfoldReflectedTile = @"CISixfoldReflectedTile";
+/**  inputImage  inputCenter  inputAngle  inputWidth */
+NSString *const LJCISixfoldRotatedTile = @"CISixfoldRotatedTile";
+/**  (三角形万花筒)inputImage  inputPoint  inputSize  inputRotation  inputDecay(衰退 腐烂) */
+NSString *const LJCITriangleKaleidoscope = @"CITriangleKaleidoscope";
+/**  inputImage  inputCenter  inputAngle  inputWidth */
+NSString *const LJCITriangleTile = @"CITriangleTile";//iOS 9
+/**  inputImage  inputCenter  inputAngle  inputWidth */
+NSString *const LJCITwelvefoldReflectedTile = @"CITwelvefoldReflectedTile";
+
+
+#pragma mark - ================ CICategoryGenerator(发生器 无inputImage) ==================
+/**  (aztec code 一种二维码) inputMessage(NSData)  inputCorrectionLevel(5~95)  inputLayers(32~1)  inputCompactStyle(0~1) */
+NSString *const LJCIAztecCodeGenerator = @"CIAztecCodeGenerator";//iOS 8
+/**  (棋盘) inputCenter  inputColor0  inputColor1  inputWidth  inputSharpness（锐度） */
+NSString *const LJCICheckerboardGenerator = @"CICheckerboardGenerator";
+/**  (条形码) inputMessage  inputQuietSpace */
+NSString *const LJCICode128BarcodeGenerator = @"CICode128BarcodeGenerator";//iOS 8
+/**  (纯色图片) inputColor  */
+NSString *const LJCIConstantColorGenerator = @"CIConstantColorGenerator";
+/**  (lenticular透镜状 halo光环)inputCenter  inputColor  inputHaloOverlap  inputHaloRadius  inputHaloWidth  inputStriationContrast  inputStriationStrength(条纹强度)  inputTime*/
+NSString *const LJCILenticularHaloGenerator = @"CILenticularHaloGenerator";//iOS 9
+/** (PDF417条码 高密度、高信息含量的) inputMessage  inputMinWidth  inputMaxWidth  inputMinHeight  inputMaxHeight  inputDataColumns  inputRows   inputPreferredAspectRatio  inputCompactionMode  inputCompactStyle  inputCorrectionLevel(0~8)  inputAlwaysSpecifyCompaction*/
+NSString *const LJCIPDF417BarcodeGenerator = @"CIPDF417BarcodeGenerator";//iOS 9
+/**  inputMessage  inputCorrectionLevel(NSString level L, M, Q, or H) */
+NSString *const LJCIQRCodeGenerator = @"CIQRCodeGenerator";//iOS 7
+/**  无属性 生成随机图 */
+NSString *const LJCIRandomGenerator = @"CIRandomGenerator";
+/**  inputCenter  inputColor  inputRadius  inputCrossScale(0~100)  inputCrossAngle  inputCrossOpacity(0~-8)  inputCrossWidth(0.5~10)  inputEpsilon(0~-8) */
+NSString *const LJCIStarShineGenerator = @"CIStarShineGenerator";
+/**  (stripes 条纹布) inputCenter  inputColor0  inputColor1  inputWidth  inputSharpness */
+NSString *const LJCIStripesGenerator = @"CIStripesGenerator";
+/** (阳光)(striation 条纹 contrast 对比) inputCenter  inputColor  inputSunRadius  inputMaxStriationRadius(0~10)  inputStriationStrength(0~3)  inputStriationContrast(0~5)  inputTime */
+NSString *const LJCISunbeamsGenerator = @"CISunbeamsGenerator";//iOS 9
+
+
+
+
+
+
+
